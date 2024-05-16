777f8853c753c5feb02d347c764ae5f0b541c6eb
Jocelyn Remington <jocelynrem@gmail.com>
2024-05-15 21:58:45 -0400

    chore: Update API base URL to use HTTPS for production environment

--- booktracker-frontend/src/config.js
+++ booktracker-frontend/src/config.js
@@ -1,8 +1,8 @@
 // src/config.js
 const config = {
     baseURL: process.env.NODE_ENV === 'production'
-        ? 'booktracker.herokuapp.com/api'
-        : 'http://127.0.0.1:8000/api'
+        ? 'https://booktracker.herokuapp.com/api'
+        : 'http://127.0.0.1:8000/api',
 };
 
 export default config;
