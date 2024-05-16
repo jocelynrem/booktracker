13059b8829165834d7da869459df3d043443e2a2
Jocelyn Remington <jocelynrem@gmail.com>
2024-05-15 22:31:26 -0400

    Update config import in Register component

--- booktracker-frontend/src/components/Register.js
+++ booktracker-frontend/src/components/Register.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import config from '../config';
+import config from '../config';  // Ensure this points to your config file
 
 const Register = ({ setLoggedIn }) => {
     const [firstName, setFirstName] = useState('');
